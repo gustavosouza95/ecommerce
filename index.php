6cbdc12f2aec053681316326c15dc6c8d94e5be4
Gustavo Souza <gustavosouza@MBP-de-Gustavo.oiwtech>
2018-04-28 23:42:08 -0300

    Categorias site

--- index.php
+++ index.php
@@ -264,6 +264,20 @@ User::verifyLogin();
 
 });
 
+$app->get("/categories/:idcategory", function($idcategory){
+
+	$category=new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new Page();
+
+	$page->setTpl("category",[
+		'category'=>$category->getValues(),
+		'products'=>[]
+	]);
+
+});
 
 $app->run();
 
---
+++ views-cache/categories-menu.58d43df53e68916f4e685c28718d335c.rtpl.php
@@ -0,0 +1,1 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/ecommerce/categories/7">Android</a></li><li><a href="/ecommerce/categories/1">Apple</a></li><li><a href="/ecommerce/categories/11">Samsung</a></li>
